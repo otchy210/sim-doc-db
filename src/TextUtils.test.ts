c4c5526721e9f7cbad22d084d2e140741691f4c1
Otchy <id+github@otchy.net>
2021-12-12 09:21:28 +0900

    0.0.10: Add toByteArray

---
+++ src/TextUtils.test.ts
@@ -0,0 +1,21 @@
+import { toByteArray } from './TextUtils';
+
+describe('toByteArray', () => {
+    it('works', () => {
+        const aBytes = toByteArray('a');
+        expect(aBytes[0]).toBe(97);
+
+        const aiuBytes = toByteArray('あいう');
+        expect(aiuBytes.length).toBe(9);
+        expect(aiuBytes[0]).toBe(227);
+        expect(aiuBytes[1]).toBe(129);
+        expect(aiuBytes[2]).toBe(130);
+
+        const smileBytes = toByteArray('😄');
+        expect(smileBytes.length).toBe(4);
+        expect(smileBytes[0]).toBe(240);
+        expect(smileBytes[1]).toBe(159);
+        expect(smileBytes[2]).toBe(152);
+        expect(smileBytes[3]).toBe(132);
+    });
+});
---
+++ src/TextUtils.ts
@@ -0,0 +1,5 @@
+const textEncoder = new TextEncoder(); // UTF-8 is default
+
+export const toByteArray = (str: string): Uint8Array => {
+    return textEncoder.encode(str);
+}
