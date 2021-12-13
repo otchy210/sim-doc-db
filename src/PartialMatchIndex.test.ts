2f1091e65de572199c3a57c953f3827a8d42d5d8
Otchy <id+github@otchy.net>
2021-12-13 15:55:55 +0900

    0.0.11: Implement double bytes support for PartialMatchIndex

--- src/PartialMatchIndex.test.ts
+++ src/PartialMatchIndex.test.ts
@@ -4,7 +4,7 @@ describe('PartialMatchIndex', () => {
     it('works with single byte case', () => {
         const index = new PartialMatchIndex();
 
-        expect(() => {index.find('あ')}).toThrowError('No multi-bytes support yet.');
+        expect(() => {index.find('あ')}).toThrowError('3 or more bytes are not supported yet.');
 
         index.add(1, ['abc', 'def']);
         index.add(2, ['ab', 'de']);
@@ -25,5 +25,44 @@ describe('PartialMatchIndex', () => {
         expect(index.find('d').size).toBe(2);
         expect(index.find('e').size).toBe(1);
         expect(index.find('f').size).toBe(0);
+    });
+
+    it('works with double bytes case', () => {
+        const index = new PartialMatchIndex();
+
+        expect(() => {index.find('あ')}).toThrowError('3 or more bytes are not supported yet.');
+
+        index.add(1, ['aabbcc', 'abcabc']);
+        index.add(2, ['aabb', 'bbcc']);
+        index.add(3, ['ab', 'bc']);
+
+        expect(index.find('ab').size).toBe(3);
+        expect(index.find('aa').size).toBe(2);
+        expect(index.find('bb').size).toBe(2);
+        expect(index.find('bc').size).toBe(3);
+        expect(index.find('cc').size).toBe(2);
+        expect(index.find('ca').size).toBe(1);
+        expect(index.find('dd').size).toBe(0);
+
+        index.remove(3);
+
+        expect(index.find('ab').size).toBe(2);
+        expect(index.find('aa').size).toBe(2);
+        expect(index.find('bb').size).toBe(2);
+        expect(index.find('bc').size).toBe(2);
+        expect(index.find('cc').size).toBe(2);
+        expect(index.find('ca').size).toBe(1);
+        expect(index.find('dd').size).toBe(0);
+
+        index.remove(1);
+        index.remove(2);
+
+        expect(index.find('ab').size).toBe(0);
+        expect(index.find('aa').size).toBe(0);
+        expect(index.find('bb').size).toBe(0);
+        expect(index.find('bc').size).toBe(0);
+        expect(index.find('cc').size).toBe(0);
+        expect(index.find('ca').size).toBe(0);
+        expect(index.find('dd').size).toBe(0);
     });
 })
--- src/PartialMatchIndex.ts
+++ src/PartialMatchIndex.ts
@@ -1,8 +1,10 @@
-class TrieNode {
-    public children = [...Array<TrieNode>(256)];
-    public ids = new Set<number>();
+import { toByteArray } from "./TextUtils";
 
-    public getChild(byte: number): TrieNode {
+class TrieNode {
+    private children = [...Array<TrieNode>(256)];
+    private ids = new Set<number>();
+
+    public getOrNewChild(byte: number): TrieNode {
         if (!this.children[byte]) {
             this.children[byte] = new TrieNode();
         }
@@ -12,34 +14,79 @@ class TrieNode {
     public add(id: number): void {
         this.ids.add(id);
     }
+
+    public get(): Set<number> {
+        return this.ids;
+    }
+
+    public removeRecursively(id: number): void {
+        this.ids.delete(id);
+        this.children
+            .filter(node => node)
+            .forEach(node => node.removeRecursively(id));
+    }
+
+    public toJson(): object {
+        const json = {} as any;
+        json['___ids___'] = Array.from(this.ids);
+        for(let i = 0; i < this.children.length; i++) {
+            const child = this.children[i];
+            if (!child) {
+                continue;
+            }
+            json[i] = child.toJson();
+        }
+        return json;
+    }
 }
 
-const encoder = new TextEncoder();
+
 
 export class PartialMatchIndex {
     private monogramRoot = new TrieNode();
+    private bigramRoot = new TrieNode();
 
     public add(id: number, values: string[]): void {
         values.forEach(value => this.addSingle(id, value));
     }
 
     private addSingle(id: number, value: string): void {
-        encoder.encode(value).forEach(byte => {
-            this.monogramRoot.getChild(byte).add(id);
-        });
+        const bytes = toByteArray(value);
+        for(let i = 0; i < bytes.length; i++) {
+            const byte = bytes[i];
+            this.monogramRoot.getOrNewChild(byte).add(id);
+            if (i < bytes.length - 1) {
+                const nextByte = bytes[i + 1];
+                this.bigramRoot
+                    .getOrNewChild(byte)
+                    .getOrNewChild(nextByte)
+                    .add(id);
+            }
+        }
     }
 
     public find(query: string): Set<number> {
-        const bytes = encoder.encode(query);
+        const bytes = toByteArray(query);
         if (bytes.length === 1) {
-            return this.monogramRoot.getChild(bytes[0]).ids;
+            return this.monogramRoot.getOrNewChild(bytes[0]).get();
         }
-        throw new Error('No multi-bytes support yet.');
+        if (bytes.length === 2) {
+            return this.bigramRoot
+                .getOrNewChild(bytes[0])
+                .getOrNewChild(bytes[1]).get();
+        }
+        throw new Error('3 or more bytes are not supported yet.');
     }
 
     public remove(id: number): void {
-        this.monogramRoot.children
-            .filter(node => node)
-            .forEach(node => node.ids.delete(id));
+        this.monogramRoot.removeRecursively(id);
+        this.bigramRoot.removeRecursively(id);
+    }
+
+    public toJsonString(): string {
+        return JSON.stringify({
+            monogram: this.monogramRoot.toJson(),
+            bigram: this.bigramRoot.toJson()
+        });
     }
 }
